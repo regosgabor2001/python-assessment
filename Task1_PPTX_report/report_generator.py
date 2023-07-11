25495f8d4a5cb31f54f3087c378a8ff523a2c875
regosgabor2001 <regosgabor2001@gmail.com>
2023-07-11 18:21:40 +0200

    Update report_generator.py

--- Task1_PPTX_report/report_generator.py
+++ Task1_PPTX_report/report_generator.py
@@ -1,15 +1,6 @@
-import argparse
 import json
 
-def read_config_file(config_file):
-    with open(config_file, 'r') as f:
-        config = json.load(f)
-    return config
+with open("sample.json", "r") as f:
+    data = json.load(f)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate a report in pptx format based on a configuration file')
-    parser.add_argument('sample.json', help='path to the configuration file')
-    args = parser.parse_args()
-
-    config = read_config_file(args.sample.json)
-    print(config)  # For testing purposes
+print(data)
